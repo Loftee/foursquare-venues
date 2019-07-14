48097435d3f3198b911310adc2117e4929f5b2e5
Andrew Collins <andrew@loftee.co.uk>
2019-07-14 15:06:16 +0100

    added shared test date file

--- src/components/VenueList.test.js
+++ src/components/VenueList.test.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import VenueList from './VenueList';
+import {testVenue} from "../testData";
 
 describe('VenueList component', () => {
-    const props = {
-        venues: [{
-            "id": "4c3d6ed94bc9a5939b07d471",
-            "name": "Costa Coffee",
-            "location": {
-                "address": "4 Great Portland St",
-                "lat": 51.51573111967288,
-                "lng": -0.14068655684295295,
-                "labeledLatLngs": [{"label": "display", "lat": 51.51573111967288, "lng": -0.14068655684295295}],
-                "distance": 42,
-                "postalCode": "W1W 8QJ",
-                "cc": "GB",
-                "city": "London",
-                "state": "Greater London",
-                "country": "United Kingdom",
-                "formattedAddress": ["4 Great Portland St", "London", "Greater London", "W1W 8QJ", "United Kingdom"]
-            }
-        }]
-    };
+    const props = { venues: [testVenue] };
     const component = shallow(<VenueList {...props} />);
     it('renders correctly', () => {
         expect(component).toMatchSnapshot();
--- src/services/services.test.js
+++ src/services/services.test.js
@@ -1,24 +1,9 @@
 import {fetchVenues} from './services';
+import {testVenue} from "../testData";
 
 describe('services', () => {
     it('fetch flight data', () => {
-        const mockResponse = [{
-            "id": "4c3d6ed94bc9a5939b07d471",
-            "name": "Costa Coffee",
-            "location": {
-                "address": "4 Great Portland St",
-                "lat": 51.51573111967288,
-                "lng": -0.14068655684295295,
-                "labeledLatLngs": [{"label": "display", "lat": 51.51573111967288, "lng": -0.14068655684295295}],
-                "distance": 42,
-                "postalCode": "W1W 8QJ",
-                "cc": "GB",
-                "city": "London",
-                "state": "Greater London",
-                "country": "United Kingdom",
-                "formattedAddress": ["4 Great Portland St", "London", "Greater London", "W1W 8QJ", "United Kingdom"]
-            }
-        }];
+        const mockResponse = [testVenue];
         fetch.mockResponseOnce(JSON.stringify({response: {venues: mockResponse}}));
         fetchVenues().then(res => {
             expect(res).toEqual(mockResponse);
